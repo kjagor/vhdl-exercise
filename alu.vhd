a5d1475267b9ff50b18ba432998db8d416173bb2
krjago <krjago@klass1-15-l.intra.itcollege.ee>
2017-12-05 18:41:12 +0200

    Patch vhdl bugs

--- alu.vhd
+++ alu.vhd
@@ -37,32 +37,29 @@ begin
 
 
     -- Here we calculate inverted bits for subtraction if necessary
-    m_inverted(0) <= not m(0);
-    m_inverted(1) <= not m(1);
-    m_inverted(2) <= not m(2);
-    m_inverted(3) <= not m(3);
-
+ m_inverted <= (not m) when sub = '1' else m;
+    
     -- Addition
     adder_instance: carry_ripple_adder
         port map(
             a => n,
             b => m_inverted,
-            ci => '1',
+            ci => sub,
             s => adder_result,
             co => adder_carry_out
         );
         
     -- Logical NAND operation
-    nand_result(0) <= not m(0) and n(0);
-    nand_result(1) <= not m(1) and n(1);
-    nand_result(2) <= not m(2) and n(2);
-    nand_result(3) <= not m(3) and n(3);
+    nand_result(0) <= not (m(0) and n(0));
+    nand_result(1) <= not (m(1) and n(1));
+    nand_result(2) <= not (m(2) and n(2));
+    nand_result(3) <= not (m(3) and n(3));
 
     -- Logical NOR operation
-    nor_result(0) <= not m(0) or n(0);
-    nor_result(1) <= not m(1) or n(1);
-    nor_result(2) <= not m(2) or n(2);
-    nor_result(3) <= not m(3) or n(3);
+    nor_result(0) <= not (m(0) or n(0));
+    nor_result(1) <= not (m(1) or n(1));
+    nor_result(2) <= not (m(2) or n(2));
+    nor_result(3) <= not (m(3) or n(3));
 
     -- Select output based on which operation was requested
     d <=   nand_result when opcode ="10" else
